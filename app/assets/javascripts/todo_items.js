1e884f11d6244a601af840fb1c326d3115252e5f
Michelle Lowe <mick_571@yahoo.com>
2013-05-01 01:21:24 -0700

    First commit

---
+++ app/assets/javascripts/todo_items.js
@@ -0,0 +1,72 @@
+// Updates/deletes immediately after new To Do created
+// Does not update/delete when page is refreshed
+
+$(document).ready(function() {
+  
+  $("#new_todo_item").on("submit", function(event){
+    event.preventDefault();
+    var form = $(this);
+    var name = $("#todo_item_name").val();
+    
+    // var completed = $("#todo_item_completed");    
+
+    $.ajax({
+      url: form.attr("action"),
+      method: form.attr("method"),
+      // input name for text field is todo_item[name]
+      data: {"todo_item": {"name": name, "completed": false} },
+      dataType: "json",
+      // register callback function on success
+      // doesn't have to be todo_item
+      success: function(todo_item) {
+        var todo_Id = todo_item.id;
+        var listTags = $("<li></li>");
+        var itemName = $("<span>" + todo_item.name + "</span>")
+        var checkBox = $("<input id='todo_item_completed' name='todo_item[completed]' type='checkbox'>");
+        var deleteBtn = $("<button class='deleteBtn'><span class='btnText'>Remove</span></button>");
+
+        $(checkBox).appendTo(listTags);
+        $(itemName).appendTo(listTags);
+        $(deleteBtn).appendTo(listTags);
+        $(listTags).appendTo("#todoItems");
+        $("#todo_item_name").val("");
+
+        checkBox.on("click", function(){
+          var chkBox = $(this);
+          if (chkBox.is(":checked")) {
+            chkBox.parent().appendTo("#completedItems").addClass("completed");
+             $.ajax({
+              url: "/todo_items/" + todo_Id,
+              method: "put",
+              dataType: "json",
+              data: {"todo_item": {"completed": true} },
+            });
+          } else {
+            chkBox.parent().appendTo("#todoItems").removeClass("completed");
+            $.ajax({
+              url: "/todo_items/" + todo_Id,
+              type: "put",
+              dataType: "json",
+              data: {"todo_item": {"completed": false} },
+            });
+          };
+        });
+
+        deleteBtn.on("click", function(){
+          var btn = $(this);
+          btn.parent().fadeOut(800, function() {btn.parent().remove()});
+          $.ajax({
+            url: "/todo_items/" + todo_Id,
+            type: "post",
+            dataType: "json",
+            data: {"_method":"delete"}
+          });
+        })
+
+      },
+      error: function(){
+        alert("Couldn't add a todo because the server was down :(");
+      }
+    });
+  });
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodolistAjax::Application.config.secret_token = '41151aa19bfe405d6f840a731f988be19a18b3dcdc22aa1765da75d83cb1ef63b5c593f06bd6fae8cfc673cc2367f26b35835893daa63c7fdebb6a5bf33ef2f2'
